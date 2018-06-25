bc6b10fa2aba1846cb16293f85d356ee457ae64c
Jacob Barbot <barbotjacob@gmail.com>
2018-06-25 22:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function happyHolidays() {
   return ("Happy holidays!")
 }
 
-function happyHolidaysTo(holiday, name) {
-  return (`Happy ${holiday}, ${name}`)
+function happyHolidaysTo (name) {
+  return (`Happy hoidays, ${name}!`)
+}
+
+function happyHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
 }
